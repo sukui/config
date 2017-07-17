29acbf0025b7583f657fcfa406c775d58e6ccad6
xiaohengjin <xiaohengjin@youzan.com>
2017-07-17 17:51:38 +0800

    init config

---
+++ src/Config.php
@@ -0,0 +1,107 @@
+<?php
+
+namespace ZanPHP\Config;
+
+use ZanPHP\Support\Arr;
+
+class Config
+{
+    private static $configMap = [];
+
+    private static function load($path, $runMode)
+    {
+        $shareConfigMap = $runModeConfig = [];
+
+        $sharePath = $path . 'share/';
+        if (is_dir($sharePath)) {
+            $shareConfigMap = ConfigLoader::getInstance()->load($sharePath);
+        }
+
+        $runModeConfigPath = $path . $runMode;
+        if (is_dir($runModeConfigPath)) {
+            $runModeConfig = ConfigLoader::getInstance()->load($runModeConfigPath);
+        }
+
+        return Arr::merge($shareConfigMap, $runModeConfig);
+    }
+
+    private static function loadPrivate($path)
+    {
+        $runMode = getenv("runMode");
+        if (!in_array($runMode, ["pre", "online"], true)) {
+            $privatePath = $path . '.private/';
+            if (is_dir($privatePath)) {
+                return ConfigLoader::getInstance()->load($privatePath);
+            }
+        }
+        return [];
+    }
+
+    public static function init()
+    {
+        $configMaps = [];
+
+        $paths = [
+            getenv("path.zan"),
+            getenv("path.config"),
+            getenv("path.module"),
+        ];
+
+        $runMode = getenv("runMode");
+        foreach ($paths as $path) {
+            $configMaps[] = self::load($path, $runMode);
+        }
+
+        foreach ($paths as $path) {
+            $configMaps[] = self::loadPrivate($path);
+        }
+
+        self::$configMap = Arr::merge(self::$configMap, ...$configMaps);
+    }
+
+    public static function get($key, $default = null)
+    {
+        $preKey = $key;
+        $routes = explode('.', $key);
+        if (empty($routes)) {
+            return $default;
+        }
+
+        $result = &self::$configMap;
+        $hasConfig = true;
+        foreach ($routes as $route) {
+            if (!isset($result[$route])) {
+                $hasConfig = false;
+                break;
+            }
+            $result = &$result[$route];
+        }
+        if (!$hasConfig) {
+            return IronConfig::get($preKey, $default);
+        }
+        return $result;
+    }
+
+    public static function set($key, $value)
+    {
+        $routes = explode('.', $key);
+        if (empty($routes)) {
+            return false;
+        }
+
+        $newConfigMap = Arr::createTreeByList($routes, $value);
+        self::$configMap = Arr::merge(self::$configMap, $newConfigMap);
+
+        return true;
+    }
+
+    public static function clear()
+    {
+        self::$configMap = [];
+    }
+
+    public static function all()
+    {
+        return self::$configMap;
+    }
+}
---
+++ src/Repository.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace ZanPHP\Config;
+
+use ZanPHP\Contracts\Config\Repository as ConfigContract;
+
+class Repository implements ConfigContract
+{
+    /**
+     * All of the configuration items.
+     *
+     * @var array
+     */
+    protected $items = [];
+
+    /**
+     * Create a new configuration repository.
+     *
+     * @param  array  $items
+     * @return void
+     */
+    public function __construct(array $items = [])
+    {
+        $this->items = $items;
+    }
+
+    /**
+     * Get the specified configuration value.
+     *
+     * @param  string  $key
+     * @param  mixed   $default
+     * @return mixed
+     */
+    public function get($key, $default = null)
+    {
+        return Config::get($key, $default);
+    }
+
+    /**
+     * Set a given configuration value.
+     *
+     * @param  array|string  $key
+     * @param  mixed   $value
+     * @return void
+     */
+    public function set($key, $value = null)
+    {
+        Config::set($key, $value);
+    }
+
+    /**
+     * Prepend a value onto an array configuration value.
+     *
+     * @param  string  $key
+     * @param  mixed  $value
+     * @return void
+     */
+    public function prepend($key, $value)
+    {
+        $array = $this->get($key);
+
+        array_unshift($array, $value);
+
+        $this->set($key, $array);
+    }
+
+    /**
+     * Push a value onto an array configuration value.
+     *
+     * @param  string  $key
+     * @param  mixed  $value
+     * @return void
+     */
+    public function push($key, $value)
+    {
+        $array = $this->get($key);
+
+        $array[] = $value;
+
+        $this->set($key, $array);
+    }
+
+    /**
+     * Get all of the configuration items for the application.
+     *
+     * @return array
+     */
+    public function all()
+    {
+        return Config::all();
+    }
+}
